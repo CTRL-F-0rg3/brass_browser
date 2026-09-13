b3f89f771c84392315bb6e42118dc305b4324081
CTRL-F-0rg3 <tdddet574@gmail.com>
2026-09-13 18:53:00 +0200

    0.1.3

--- src/browser/engine.rs
+++ src/browser/engine.rs
@@ -1,63 +1,12 @@
-use servo::{
-    DeviceIntPoint, DeviceIntRect, DevicePoint, EventLoopWaker, InputEvent, LoadStatus, MouseButton,
-    MouseButtonAction, RenderingContext, Servo, ServoBuilder,
-    SoftwareRenderingContext, WebView, WebViewBuilder, WebViewDelegate, WebViewPoint,
-    WheelDelta, WheelMode,
+use tao::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
 };
-use std::rc::Rc;
-use std::sync::{
-    atomic::{AtomicBool, Ordering},
-    Arc,
-};
-
-struct EguiEventLoopWaker {
-    needs_repaint: Arc<AtomicBool>,
-}
-
-impl EventLoopWaker for EguiEventLoopWaker {
-    fn wake(&self) {
-        self.needs_repaint.store(true, Ordering::SeqCst);
-    }
-    fn clone_box(&self) -> Box<dyn EventLoopWaker> {
-        Box::new(Self {
-            needs_repaint: Arc::clone(&self.needs_repaint),
-        })
-    }
-}
-
-struct BrowserWebViewDelegate {
-    needs_repaint: Arc<AtomicBool>,
-    is_loading: Arc<AtomicBool>,
-}
-
-impl WebViewDelegate for BrowserWebViewDelegate {
-    fn notify_new_frame_ready(&self, webview: WebView) {
-        self.needs_repaint.store(true, Ordering::SeqCst);
-        webview.paint();
-    }
-
-    fn notify_load_status(&self, _webview: WebView, status: LoadStatus) {
-        match status {
-            LoadStatus::Started => {
-                self.is_loading.store(true, Ordering::SeqCst);
-                self.needs_repaint.store(true, Ordering::SeqCst);
-            }
-            LoadStatus::Complete => {
-                self.is_loading.store(false, Ordering::SeqCst);
-                println!("[Engine] Strona załadowana!");
-            }
-            _ => {}
-        }
-    }
-}
+use wry::{http::Request, WebViewBuilder};
 
 pub struct BrowserEngine {
     pub current_url: String,
-    pub servo: Option<Servo>,
-    pub webview: Option<WebView>,
-    pub rendering_context: Option<Rc<SoftwareRenderingContext>>,
-    needs_repaint: Arc<AtomicBool>,
-    is_loading: Arc<AtomicBool>,
     pub width: u32,
     pub height: u32,
 }
@@ -66,172 +15,59 @@ impl BrowserEngine {
     pub fn new(initial_url: &str) -> Self {
         Self {
             current_url: initial_url.to_string(),
-            servo: None,
-            webview: None,
-            rendering_context: None,
-            needs_repaint: Arc::new(AtomicBool::new(true)),
-            is_loading: Arc::new(AtomicBool::new(true)),
             width: 1280,
             height: 720,
         }
     }
 
-    pub fn init(&mut self) {
-        println!("[Engine] Inicjalizacja Servo...");
-        let needs_repaint = Arc::clone(&self.needs_repaint);
-        let is_loading = Arc::clone(&self.is_loading);
+    pub fn run(&mut self) {
+        println!("[Engine] Uruchamianie z wry...");
+        
+        let event_loop = EventLoop::new();
+        let window = WindowBuilder::new()
+            .with_title("Brass Browser")
+            .with_inner_size(tao::dpi::LogicalSize::new(self.width as f64, self.height as f64))
+            .build(&event_loop)
+            .unwrap();
 
-        let _waker = EguiEventLoopWaker {
-            needs_repaint: needs_repaint.clone(),
+        let url = if self.current_url.starts_with("http") {
+            self.current_url.clone()
+        } else {
+            format!("https://{}", self.current_url)
         };
 
-        let builder = ServoBuilder::default();
-        let servo_inst = builder.build();
-        println!("[Engine] ✓ Servo zbudowane!");
+        println!("[Engine] Ładowanie: {}", url);
 
-        let size = dpi::PhysicalSize::new(self.width, self.height);
-        let rendering_context = SoftwareRenderingContext::new(size)
-            .expect("Nie udało się utworzyć SoftwareRenderingContext");
-        let rc_context: Rc<SoftwareRenderingContext> = Rc::new(rendering_context);
-        println!("[Engine] ✓ RenderingContext utworzony!");
+        let webview = WebViewBuilder::new(&window)
+            .unwrap()
+            .with_url(&url)
+            .unwrap()
+            .build()
+            .unwrap();
 
-        let delegate = BrowserWebViewDelegate {
-            needs_repaint: needs_repaint.clone(),
-            is_loading: is_loading.clone(),
-        };
-
-        let rc_trait: Rc<dyn RenderingContext> = rc_context.clone();
-        let webview_builder = WebViewBuilder::new(&servo_inst, rc_trait)
-            .delegate(Rc::new(delegate));
-
-        let webview = webview_builder.build();
         println!("[Engine] ✓ WebView utworzony!");
 
-        self.servo = Some(servo_inst);
-        self.webview = Some(webview);
-        self.rendering_context = Some(rc_context);
+        event_loop.run(move |event, _, control_flow| {
+            *control_flow = ControlFlow::Wait;
 
-        let initial_url = self.current_url.clone();
-        self.navigate(&initial_url);
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                Event::WindowEvent {
+                    event: WindowEvent::Resized(size),
+                    ..
+                } => {
+                    let _ = webview.resize();
+                }
+                _ => {}
+            }
+        });
     }
 
     pub fn navigate(&mut self, url: &str) {
-        let formatted_url = if !url.starts_with("http://") && !url.starts_with("https://") {
-            format!("https://{}", url)
-        } else {
-            url.to_string()
-        };
-
-        println!("[Engine] Nawigacja do: {}", formatted_url);
-        self.current_url = formatted_url.clone();
-        self.needs_repaint.store(true, Ordering::SeqCst);
-        self.is_loading.store(true, Ordering::SeqCst);
-
-        if let (Some(webview), Some(servo)) = (&self.webview, &self.servo) {
-            if let Ok(parsed_url) = url::Url::parse(&formatted_url) {
-                println!("[Engine] Ładowanie URL: {}", parsed_url);
-                webview.load(parsed_url);
-                servo.spin_event_loop();
-            }
-        }
-    }
-
-    pub fn current_url(&self) -> &str {
-        &self.current_url
-    }
-
-    pub fn needs_repaint(&self) -> bool {
-        self.needs_repaint.load(Ordering::SeqCst)
-    }
-
-    pub fn is_loading(&self) -> bool {
-        self.is_loading.load(Ordering::SeqCst)
-    }
-
-    pub fn clear_repaint_flag(&self) {
-        self.needs_repaint.store(false, Ordering::SeqCst);
-    }
-
-    pub fn handle_mouse_button(&mut self, button: u8, pressed: bool, x: f32, y: f32) {
-        if let (Some(webview), Some(servo)) = (&self.webview, &self.servo) {
-            let servo_button = match button {
-                0 => MouseButton::Primary,
-                1 => MouseButton::Secondary,
-                2 => MouseButton::Auxiliary,
-                _ => return,
-            };
-            let action = if pressed { MouseButtonAction::Down } else { MouseButtonAction::Up };
-            let point = WebViewPoint::Device(DevicePoint::new(x, y));
-            let event = InputEvent::MouseButton(servo::MouseButtonEvent {
-                action, button: servo_button, point,
-            });
-            webview.notify_input_event(event);
-            servo.spin_event_loop();
-            self.needs_repaint.store(true, Ordering::SeqCst);
-        }
-    }
-
-    pub fn handle_mouse_move(&mut self, x: f32, y: f32) {
-        if let Some(webview) = &self.webview {
-            let point = WebViewPoint::Device(DevicePoint::new(x, y));
-            let event = InputEvent::MouseMove(servo::MouseMoveEvent {
-                point,
-                is_compatibility_event_for_touch: false,
-            });
-            webview.notify_input_event(event);
-        }
-    }
-
-    pub fn handle_wheel(&mut self, delta_x: f32, delta_y: f32, x: f32, y: f32) {
-        if let (Some(webview), Some(servo)) = (&self.webview, &self.servo) {
-            let point = WebViewPoint::Device(DevicePoint::new(x, y));
-            let event = InputEvent::Wheel(servo::WheelEvent {
-                delta: WheelDelta {
-                    x: delta_x as f64,
-                    y: delta_y as f64,
-                    z: 0.0,
-                    mode: WheelMode::DeltaLine,
-                },
-                point,
-            });
-            webview.notify_input_event(event);
-            servo.spin_event_loop();
-            self.needs_repaint.store(true, Ordering::SeqCst);
-        }
-    }
-
-    pub fn resize(&mut self, width: u32, height: u32) {
-        if width == self.width && height == self.height {
-            return;
-        }
-        println!("[Engine] Resize: {}x{}", width, height);
-        self.width = width;
-        self.height = height;
-        self.needs_repaint.store(true, Ordering::SeqCst);
-
-        if let Some(context) = &self.rendering_context {
-            context.resize(dpi::PhysicalSize::new(width, height));
-        }
-    }
-
-    pub fn render_frame(&mut self) -> Option<Vec<u8>> {
-        if let (Some(webview), Some(servo), Some(context)) = (
-            &self.webview,
-            &self.servo,
-            &self.rendering_context,
-        ) {
-            webview.paint();
-            servo.spin_event_loop();
-            context.present();
-
-            let min_point = DeviceIntPoint::new(0, 0);
-            let max_point = DeviceIntPoint::new(self.width as i32, self.height as i32);
-            let rect = DeviceIntRect::new(min_point, max_point);
-
-            if let Some(rgba_image) = context.read_to_image(rect) {
-                return Some(rgba_image.into_raw());
-            }
-        }
-        None
+        self.current_url = url.to_string();
+        println!("[Engine] Nawigacja do: {}", url);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,149 +1,76 @@
-mod browser;
-mod ui;
-use browser::engine::BrowserEngine;
-use ui::navbar::NavBar;
 use eframe::egui;
+use tao::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use wry::{http::Request, WebViewBuilder};
+use std::sync::{Arc, Mutex};
 
-struct BrassBrowserApp {
-    engine: BrowserEngine,
-    navbar: NavBar,
-    gl_texture_handle: Option<egui::TextureHandle>,
-    frame_width: usize,
-    frame_height: usize,
-    browser_rect: Option<egui::Rect>,
-    last_load_time: std::time::Instant,
+struct BrowserApp {
+    url_input: String,
+    current_url: String,
+    webview_ready: Arc<Mutex<bool>>,
 }
 
-impl BrassBrowserApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        let mut engine = BrowserEngine::new("https://trangorgeos.website");
-        engine.init();
-
-        let navbar = NavBar::new(engine.current_url());
-        let width = 1280;
-        let height = 720;
-        
-        let pixels = vec![255u8; width * height * 4];
-        let image = egui::ColorImage::from_rgba_unmultiplied([width, height], &pixels);
-        let handle = cc.egui_ctx.load_texture("browser_canvas", image, egui::TextureOptions::LINEAR);
-        
+impl BrowserApp {
+    fn new() -> Self {
         Self {
-            engine,
-            navbar,
-            gl_texture_handle: Some(handle),
-            frame_width: width,
-            frame_height: height,
-            browser_rect: None,
-            last_load_time: std::time::Instant::now(),
+            url_input: "https://trangorgeos.website".to_string(),
+            current_url: "https://trangorgeos.website".to_string(),
+            webview_ready: Arc::new(Mutex::new(false)),
         }
     }
 
-    fn update_browser_frame(&mut self) -> bool {
-        // Renderuj gdy: strona się ładuje LUB Servo potrzebuje LUB minęło < 30s od ostatniej nawigacji
-        let should_render = self.engine.is_loading() 
-            || self.engine.needs_repaint() 
-            || self.last_load_time.elapsed().as_secs() < 30;
-        
-        if should_render {
-            if let Some(pixels) = self.engine.render_frame() {
-                let image = egui::ColorImage::from_rgba_unmultiplied(
-                    [self.frame_width, self.frame_height],
-                    &pixels,
-                );
-                if let Some(texture) = &mut self.gl_texture_handle {
-                    texture.set(image, egui::TextureOptions::LINEAR);
-                }
-                self.engine.clear_repaint_flag();
-                return true;
-            }
-        }
-        false
-    }
-    
-    fn is_mouse_over_browser(&self, pos: egui::Pos2) -> bool {
-        if let Some(rect) = self.browser_rect {
-            rect.contains(pos)
-        } else {
-            false
-        }
+    fn navigate(&mut self, url: &str) {
+        self.current_url = url.to_string();
+        self.url_input = url.to_string();
+        println!("[UI] Nawigacja do: {}", url);
     }
 }
 
-impl eframe::App for BrassBrowserApp {
+impl eframe::App for BrowserApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        if let Some(new_url) = self.navbar.render(ctx) {
-            self.engine.navigate(&new_url);
-            self.last_load_time = std::time::Instant::now();
-        }
-
-        let raw_events = ctx.input(|i| i.raw.events.clone());
-        let pointer_pos = ctx.input(|i| i.pointer.latest_pos());
-
-        for event in raw_events {
-            match event {
-                egui::Event::PointerMoved(pos) => {
-                    if self.is_mouse_over_browser(pos) {
-                        if let Some(rect) = self.browser_rect {
-                            self.engine.handle_mouse_move(pos.x - rect.min.x, pos.y - rect.min.y);
-                        }
-                    }
+        // Navbar
+        egui::TopBottomPanel::top("nav_bar").show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                if ui.button("").clicked() {
+                    println!("[UI] Wstecz");
                 }
-                egui::Event::PointerButton { pos, button, pressed, .. } => {
-                    if self.is_mouse_over_browser(pos) {
-                        if let Some(rect) = self.browser_rect {
-                            let btn = match button {
-                                egui::PointerButton::Primary => 0,
-                                egui::PointerButton::Secondary => 1,
-                                egui::PointerButton::Middle => 2,
-                                _ => 255,
-                            };
-                            self.engine.handle_mouse_button(btn, pressed, pos.x - rect.min.x, pos.y - rect.min.y);
-                        }
-                    }
+                if ui.button("►").clicked() {
+                    println!("[UI] Dalej");
                 }
-                egui::Event::Scroll(delta) => {
-                    if let Some(pos) = pointer_pos {
-                        if self.is_mouse_over_browser(pos) {
-                            if let Some(rect) = self.browser_rect {
-                                self.engine.handle_wheel(delta.x, delta.y, pos.x - rect.min.x, pos.y - rect.min.y);
-                            }
-                        }
-                    }
+                if ui.button("🔄").clicked() {
+                    println!("[UI] Odśwież");
                 }
-                _ => {}
-            }
-        }
-
-        self.update_browser_frame();
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            let available_size = ui.available_size();
-            
-            // Dopasuj rozmiar renderingu Servo do rzeczywistego rozmiaru okna
-            if available_size.x as u32 != self.frame_width as u32 || 
-               available_size.y as u32 != self.frame_height as u32 {
-                self.frame_width = available_size.x as usize;
-                self.frame_height = available_size.y as usize;
-                self.engine.resize(available_size.x as u32, available_size.y as u32);
-            }
-            
-            let response = ui.add_sized(
-                available_size,
-                egui::Image::new(self.gl_texture_handle.as_ref().unwrap())
-                    .fit_to_exact_size(available_size)
-            );
-            self.browser_rect = Some(response.rect);
+                
+                ui.add_sized(
+                    ui.available_size(),
+                    egui::TextEdit::singleline(&mut self.url_input)
+                        .hint_text("Wpisz URL..."),
+                );
+                
+                if ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                    let url = self.url_input.clone();
+                    self.navigate(&url);
+                }
+            });
         });
 
-        // Renderuj gdy strona się ładuje lub są zmiany
-        if self.engine.is_loading() || self.engine.needs_repaint() || self.last_load_time.elapsed().as_secs() < 30 {
-            ctx.request_repaint();
-        }
+        // Główna zawartość - tutaj powinien być WebView
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("WebView będzie tutaj...");
+            ui.label(format!("Aktualny URL: {}", self.current_url));
+        });
+
+        ctx.request_repaint();
     }
 }
 
 fn main() -> eframe::Result<()> {
     env_logger::init();
+    
+    println!("[Brass Browser] Uruchamianie...");
     
     let options = eframe::NativeOptions {
         renderer: eframe::Renderer::Glow,
@@ -153,11 +80,9 @@ fn main() -> eframe::Result<()> {
         ..Default::default()
     };
     
-    println!("[Brass Browser] Uruchamianie...");
-    
     eframe::run_native(
         "Brass Browser",
         options,
-        Box::new(|cc| Box::new(BrassBrowserApp::new(cc))),
+        Box::new(|_cc| Box::new(BrowserApp::new())),
     )
 }
